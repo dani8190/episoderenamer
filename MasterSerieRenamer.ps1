5e9412e010dee5b639d19d2823cd7f9a45b1969a
Daniel Søgaard <nothingisrealmedia@hotmail.com>
2023-06-03 22:13:25 +0200

    Add files via upload

---
+++ MasterSerieRenamer.ps1
@@ -0,0 +1,33 @@
+﻿cls;
+$list = Get-ChildItem;
+
+
+foreach ($0s in $list) {
+
+
+    #check if episode has not been sorted already
+    if ($0s -notmatch "S01") {
+
+        #taking found one, reading its number, and adding S01E-whatever number to the name
+
+        $originalName = $0s;
+        $Dissaemble1 = $originalName -replace ".mp4", ''; #assuming its mp4
+        $EpisodeNumber = $Dissaemble1 -replace "[^0-9]" , ''; # assuming the number of the episode is within the filename.
+
+
+        #change S01 to whatever season you are working with.
+        $frontNum = "S01E"+$EpisodeNumber;
+
+        #adding S01E.... to the original filename
+        $newName = $frontNum + " - " + $originalName
+
+        #renameing file
+        Rename-Item -Path $0s -NewName $newName
+
+        Write-Host "$0s is now called $newname";
+
+        
+    
+    }
+
+}
